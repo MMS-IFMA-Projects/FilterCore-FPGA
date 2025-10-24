74c7b320d80fad6e07dba9263eb67ab05d2b2222
Mayron-Martins <mayronmartins013@gmail.com>
2025-10-24 15:00:56 -0300

    pH calibration

--- Project_Raspberry/src/components/analog/ph4502c.c
+++ Project_Raspberry/src/components/analog/ph4502c.c
@@ -2,11 +2,13 @@
 #include "ads1115.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include <stdio.h>
+
 
 #define PH_ADC_CHANNEL 0
 #define NUM_SAMPLES 10
-#define CALIBRATION_OFFSET 20.5f
-#define VOLTS_TO_PH_SLOPE -5.70f
+#define CALIBRATION_OFFSET 58.2470f
+#define VOLTS_TO_PH_SLOPE -20.4082f
 
 static void sort_samples(int16_t* samples) {
     // Simple bubble sort
@@ -21,10 +23,47 @@ static void sort_samples(int16_t* samples) {
     }
 }
 
+// Calibration
+static float ph4502c_read_voltage(void) {
+    int16_t samples[NUM_SAMPLES];
+    int32_t total_raw_adc = 0;
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+        samples[i] = ads1115_read_adc(PH_ADC_CHANNEL);
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+    sort_samples(samples);
+    for (int i = 2; i < NUM_SAMPLES - 2; i++) {
+        total_raw_adc += samples[i];
+    }
+    int16_t avg_sample = total_raw_adc / (NUM_SAMPLES - 4);
+    float voltage = (float)avg_sample * (ADS1115_VREF / ADS1115_MAX_ADC_VALUE);
+
+    // Imprima a tensão para você ver no console
+    printf("Tensão do ADC: %.4f V\n", voltage);
+
+    return voltage;
+}
+
+static void ph4502c_calibrate(void) {
+    float acid_ph = 4.2f; //replace with acid pH
+    float base_ph = 6.9f; //replace with base pH
+
+    ph4502c_read_voltage();
+
+    float voltage_at_acid_ph = 2.6532f; //replace with voltage at acid pH
+    float voltage_at_base_ph = 2.5160f; //replace with voltage at base pH
+
+    float slope = (base_ph - acid_ph) / (voltage_at_base_ph - voltage_at_acid_ph);
+    float offset = acid_ph - (slope * voltage_at_acid_ph);
+
+    printf("Slope: %.4f\n", slope);
+    printf("Offset: %.4f\n", offset);
+}
+
 ph_t ph4502c_read_ph(void) {
     int16_t samples[NUM_SAMPLES];
     int32_t total_raw_adc = 0;
-
+    
     // Collects multiple samples from the ADC to reduce noise
     for (int i = 0; i < NUM_SAMPLES; i++) {
         samples[i] = ads1115_read_adc(PH_ADC_CHANNEL);
