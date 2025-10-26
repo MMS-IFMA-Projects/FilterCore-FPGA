63ed8c6365ab581800016f81877867eb2ca7e269
Mayron-Martins <mayronmartins013@gmail.com>
2025-10-26 11:42:43 -0300

    FCF-68 <Water level implementation with debouncer>

--- Project_FPGA/filter_fsm.sv
+++ Project_FPGA/filter_fsm.sv
@@ -11,97 +11,97 @@ module filter_fsm(
     output logic [7:0] pwm_duty_b
 );
 
-// --- Parameters ---
-localparam PWM_MAX = 8'd230;
-localparam PWM_MIN = 8'd77;
-localparam int PUMP_B_TIMER_CYCLES = 250_000_000; // 5 seconds @ 50 MHz
-localparam int PUMP_A_FILL_TIME_CYCLES 6_000_000_000; // 2 minutes @ 50 MHz
+    // --- Parameters ---
+    localparam PWM_MAX = 8'd230;
+    localparam PWM_MIN = 8'd77;
+    localparam int PUMP_B_TIMER_CYCLES = 250_000_000; // 5 seconds @ 50 MHz
+    localparam int PUMP_A_FILL_TIME_CYCLES 6_000_000_000; // 2 minutes @ 50 MHz
 
-// --- FSM States ---
-typedef enum logic {
-    STOP,
-    FILLING,
-    DRAINING_MIN,
-    DRAINING_MAX,
-    STOPPING
-} state_t;
+    // --- FSM States ---
+    typedef enum logic {
+        STOP,
+        FILLING,
+        DRAINING_MIN,
+        DRAINING_MAX,
+        STOPPING
+    } state_t;
 
-state_t current_state, next_state;
+    state_t current_state, next_state;
 
-// --- Timers ---
-logic [29:0] timer_pump_a;
-logic [27:0] timer_pump_b;
-logic pump_a_timer_expired;
-logic pump_b_timer_expired;
+    // --- Timers ---
+    logic [29:0] timer_pump_a;
+    logic [27:0] timer_pump_b;
+    logic pump_a_timer_expired;
+    logic pump_b_timer_expired;
 
-// --- Pump A Timer (Fill Timer) ---
-always_ff @(posedge clk or posedge reset) begin
-    if (reset) timer_pump_a <= '0;
-    else if (current_state == FILLING) begin
-        if(!pump_a_timer_expired) timer_pump_a <= timer_pump_a + 1;
-        else timer_pump_a <= '0;
-    end
-end
-
-assign pump_a_timer_expired = (timer_pump_a >= PUMP_A_FILL_TIME_CYCLES);
-
-// --- Pump B Timer (Min/Max Timer) ---
-always_ff @(posedge clk or posedge reset) begin
-    if (reset) timer_pump_b <= '0;
-    else if (current_state == DRAINING_MIN && next_state != DRAINING_MIN) timer_pump_b <= '0;
-    else if (current_state == DRAINING_MIN) begin
-        if(!pump_b_timer_expired) timer_pump_b <= timer_pump_b + 1;
-        else timer_pump_b <= '0;
-    end
-end
-
-assign pump_b_timer_expired = (timer_pump_b >= PUMP_B_TIMER_CYCLES);
-
-// --- FSM State Transition Logic ---
-always_comb begin
-    next_state = current_state;
-    logic is_critical = (|status_data);
-
-    case (current_state)
-        STOP:
-            if (is_critical) next_state = FILLING;
-        FILLING:
-            if (pump_a_timer_expired) next_state = DRAINING_MIN;
-        DRAINING_MIN:
-            if (!is_critical) next_state = STOPPING;
-            else if (pump_b_timer_expired) next_state = DRAINING_MAX;
-            else if (is_empty && is_critical) next_state = FILLING;
-        DRAINING_MAX:
-            if (!is_critical) next_state = STOPPING;
-            else if (is_empty && !is_critical) next_state = FILLING;
-        STOPPING:
-            if (is_empty) next_state = STOP;
-        default:
-            next_state = STOP;
-
-    endcase
-end
-
-// --- FSM State Register ---
-always_ff @(posedge clk or posedge reset) begin
-    if (reset) current_state <= STOP;
-    else current_state <= next_state;
-end
-
-// --- FSM Output (Pump Control) ---
-always_comb begin
-    pwm_duty_a = 8'h00;
-    pwm_duty_b = 8'h00;
-
-    case (current_state)
-        FILLING: pwm_duty_a = PWM_MAX;
-        DRAINING_MIN: pwm_duty_b = PWM_MIN;
-        DRAINING_MAX: pwm_duty_b = PWM_MAX;
-        STOPPING: pwm_duty_b = PWM_MAX;
-        default: begin
-            pwm_duty_a = 8'h00;
-            pwm_duty_b = 8'h00;
+    // --- Pump A Timer (Fill Timer) ---
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) timer_pump_a <= '0;
+        else if (current_state == FILLING) begin
+            if(!pump_a_timer_expired) timer_pump_a <= timer_pump_a + 1;
+            else timer_pump_a <= '0;
         end
-    endcase
-end
+    end
+
+    assign pump_a_timer_expired = (timer_pump_a >= PUMP_A_FILL_TIME_CYCLES);
+
+    // --- Pump B Timer (Min/Max Timer) ---
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) timer_pump_b <= '0;
+        else if (current_state == DRAINING_MIN && next_state != DRAINING_MIN) timer_pump_b <= '0;
+        else if (current_state == DRAINING_MIN) begin
+            if(!pump_b_timer_expired) timer_pump_b <= timer_pump_b + 1;
+            else timer_pump_b <= '0;
+        end
+    end
+
+    assign pump_b_timer_expired = (timer_pump_b >= PUMP_B_TIMER_CYCLES);
+
+    // --- FSM State Transition Logic ---
+    always_comb begin
+        next_state = current_state;
+        logic is_critical = (|status_data);
+
+        case (current_state)
+            STOP:
+                if (is_critical) next_state = FILLING;
+            FILLING:
+                if (pump_a_timer_expired) next_state = DRAINING_MIN;
+            DRAINING_MIN:
+                if (!is_critical) next_state = STOPPING;
+                else if (pump_b_timer_expired) next_state = DRAINING_MAX;
+                else if (is_empty && is_critical) next_state = FILLING;
+            DRAINING_MAX:
+                if (!is_critical) next_state = STOPPING;
+                else if (is_empty && !is_critical) next_state = FILLING;
+            STOPPING:
+                if (is_empty) next_state = STOP;
+            default:
+                next_state = STOP;
+
+        endcase
+    end
+
+    // --- FSM State Register ---
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) current_state <= STOP;
+        else current_state <= next_state;
+    end
+
+    // --- FSM Output (Pump Control) ---
+    always_comb begin
+        pwm_duty_a = 8'h00;
+        pwm_duty_b = 8'h00;
+
+        case (current_state)
+            FILLING: pwm_duty_a = PWM_MAX;
+            DRAINING_MIN: pwm_duty_b = PWM_MIN;
+            DRAINING_MAX: pwm_duty_b = PWM_MAX;
+            STOPPING: pwm_duty_b = PWM_MAX;
+            default: begin
+                pwm_duty_a = 8'h00;
+                pwm_duty_b = 8'h00;
+            end
+        endcase
+    end
 endmodule
---
+++ Project_FPGA/water_level.sv
@@ -0,0 +1,37 @@
+module water_level (
+    input wire clk,
+    input wire  reset,
+    input wire signal_async,
+    output logic signal_stable
+);
+    // --- Parameters ---
+    localparam int CLK_FREQ = 50_000_000; //50MHz Clock
+    localparam int STABLE_MS = 20;        //20ms Stable Time
+    localparam int COUNTER_LIMIT = (CLK_FREQ / 1000) * STABLE_MS;
+    
+    // --- Counting and validation ---
+    logic [19:0] counter = '0;
+    logic signal_sync;
+
+    // --- 2-stage synchronizer ---
+    always_ff @(posedge clk) begin
+        signal_sync <= signal_async;
+    end
+
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            signal_stable <= 1'b1; // Default to 'empty'
+            counter <= '0;
+        end
+        else begin
+            if (signal_sync != signal_stable) begin
+                if(counter < COUNTER_LIMIT) counter <= counter + 1;
+                else begin
+                    signal_stable <= signal_sync;
+                    counter <= '0;
+                end
+            end
+            else counter <= '0;
+        end
+    end
+endmodule
