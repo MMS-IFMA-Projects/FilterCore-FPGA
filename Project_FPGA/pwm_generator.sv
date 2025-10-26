f53c675db1fe0cc3f7beafd08ad03de1e2b53695
Mayron-Martins <mayronmartins013@gmail.com>
2025-10-26 11:48:12 -0300

    FCF-59 <PWM Simplification>

--- Project_FPGA/pwm_generator.sv
+++ Project_FPGA/pwm_generator.sv
@@ -5,20 +5,17 @@
 module pwm_generator #(
     parameter int WIDTH = 8
 ) (
-    input wire clk, // Renamed for consistency
-    input wire reset, // Active-high reset
-    input wire [WIDTH-1:0] duty_cycle_val,
+    input wire clk, 
+    input wire reset, 
+    input wire [WIDTH-1:0] duty_cycle,
     output logic pwm_signal
 );
     logic [WIDTH-1:0] pwm_counter = '0;
 
     always_ff @(posedge clk or posedge reset) begin
-        if (reset) begin
-            pwm_counter <= '0;
-        end else begin
-            pwm_counter <= pwm_counter + 1;
-        end
+        if (reset) pwm_counter <= '0;
+        else pwm_counter <= pwm_counter + 1;
     end
 
-    assign pwm_signal = (pwm_counter < duty_cycle_val);
+    assign pwm_signal = (pwm_counter < duty_cycle);
 endmodule
