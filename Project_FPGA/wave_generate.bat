c17296a871b9c0d16f8126d4244d8760a48e647f
Mayron-Martins <mayronmartins013@gmail.com>
2025-10-27 14:58:00 -0300

    FCF-4 <Changes to the bat executable>

--- Project_FPGA/wave_generate.bat
+++ Project_FPGA/wave_generate.bat
@@ -5,7 +5,7 @@ echo.
 
 REM --- Step 1: Compile all .sv files and create the simulation executable ---
 echo [STEP 1/2] Compiling the project...
-iverilog -g2012 -o designer.vvp handshake_receiver.sv pwm_generator.sv designer.sv tb_designer.sv
+iverilog -g2012 -o design.vvp tb_design.sv design.sv filter_fsm.sv handshake_fsm.sv water_level.sv pwm_generator.sv
 
 REM Check if compilation failed
 IF %ERRORLEVEL% NEQ 0 (
@@ -16,11 +16,13 @@ IF %ERRORLEVEL% NEQ 0 (
 
 REM --- Step 2: Run the simulation ---
 echo [STEP 2/2] Running the simulation...
-vvp designer.vvp
+vvp design.vvp
 
 echo.
 echo --------------------------------------------------------------------
-echo [SUCCESS] Simulation completed! The 'onda.vcd' file has been generated.
+echo [SUCCESS] Simulation completed! The 'design.vcd' file has been generated.
 echo --------------------------------------------------------------------
+
+gtkwave design.vcd
 
 pause
